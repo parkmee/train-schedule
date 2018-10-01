f5f26f6faea93d587728590e0089744ddb290f94
MMPark <parkmee@gmail.com>
2018-10-01 19:07:42 -0400

    completed logic and updated UI

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # train-schedule
+
+This application store train information on a Firebase server to host arrival and departure data of trains
+
+Functions:
+- enter train name, destination, first train time (military time), and frequency in minutes
+- the information for the trains by chronological time of arrival will show
---
+++ assets/javascript/app.js
@@ -0,0 +1,73 @@
+$(document).ready(function(){
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDJ9v4tSZ43vWLLUkVfsKXo2hURB36EczQ",
+        authDomain: "fir-demo20180922.firebaseapp.com",
+        databaseURL: "https://fir-demo20180922.firebaseio.com",
+        projectId: "fir-demo20180922",
+        storageBucket: "fir-demo20180922.appspot.com",
+        messagingSenderId: "954494375323"
+    };
+
+    firebase.initializeApp(config);
+
+    const db = firebase.database();
+
+    var trainName = "";
+    var destination = "";
+    var trainStart = "";
+    var trainFreq = "";
+    var minAway = 0;
+    
+    $("#submit").on("click", function () {
+    
+        trainName = $("#train-name").val().trim();
+        destination = $("#destination").val().trim();
+        trainStart = $("#train-start").val().trim();
+        trainFreq = $("#train-freq").val().trim();
+    
+        db.ref('train-schedule').push({
+            trainName,
+            destination,
+            trainStart,
+            trainFreq,
+            minAway,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+    });
+    
+    db.ref('train-schedule').on("child_added", function(childSnapshot){
+        const sv = childSnapshot.val();
+        
+        console.log(sv.trainName);
+        trainName = sv.trainName;
+        destination = sv.destination;
+        trainFreq = sv.trainFreq;
+        trainStart = sv.trainStart;
+
+        startConvert = moment(trainStart, "HH:mm");
+        const now = moment();
+        const diffTime = now.diff(moment(startConvert), "minutes");
+        const minAway = trainFreq - (diffTime % trainFreq);
+        const nextArrival = now.add(minAway, "minutes").format("hh:mm");
+    
+        $("tbody").prepend(
+            `
+            <tr>
+                <td class="text-center">${trainName}</td>
+                <td class="text-center">${destination}</td>
+                <td class="text-center">${trainFreq}</td>
+                <td class="text-center">${nextArrival}</td>
+                <td class="text-center">${minAway}</td>
+            </tr>
+            `
+        );
+    }, function(errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+    });
+
+});
+
+
+
+
